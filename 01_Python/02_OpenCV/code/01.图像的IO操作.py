5580109a08a4cd63e3bec1ad502c80e2f5ebf222
fafa <fafa27182818@gmail.com>
2024-06-16 01:09:14 +0800

    OpenCV

---
+++ 01_Python/02_OpenCV/code/01.图像的IO操作.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python 3.10
+# -*- coding: utf-8 -*- 
+#
+# @Time    : 2023-11-06 21:19
+# @Author  : 发发
+# @QQ      : 1315337973
+# @GitHub  : https://github.com/lovely-fafa
+# @File    : 01.图像的IO操作.py
+# @Software: PyCharm
---
+++ 01_Python/02_OpenCV/code/02_图像绘制图形.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python 3.10
+# -*- coding: utf-8 -*- 
+#
+# @Time    : 2023-11-07 14:16
+# @Author  : 发发
+# @QQ      : 1315337973
+# @GitHub  : https://github.com/lovely-fafa
+# @File    : 02_图像绘制图形.py
+# @Software: PyCharm
